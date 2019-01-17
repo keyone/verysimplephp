619d41b2b26d6d6ab2aad345fb3769ddcc0f6238
Kevin Emamirad <keyone@gmail.com>
2019-01-17 00:14:28 -0500

    simple app

---
+++ README.md
@@ -0,0 +1,8 @@
+# Very Simple PHP application
+
+To run it just use your PHP cli.
+
+```
+php -S localhost:4000
+```
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<p>That's  a very simple application</p>";
+echo "<strong> &copy; 2019. Tr&egrave;s simple";
+
